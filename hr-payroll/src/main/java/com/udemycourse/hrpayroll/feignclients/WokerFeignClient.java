a3355002d36afa5d93d544c55f604d6aee0befaf
Denner <dennerramiror@gmail.com>
2022-03-23 16:29:16 -0300

    Balanceamento de Carga com Ribbon

--- hr-payroll/src/main/java/com/udemycourse/hrpayroll/feignclients/WokerFeignClient.java
+++ hr-payroll/src/main/java/com/udemycourse/hrpayroll/feignclients/WokerFeignClient.java
@@ -8,11 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.udemycourse.hrpayroll.entities.Worker;
 
-
-
-
 @Component
-@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+@FeignClient(name = "hr-worker", path = "/workers")
 public interface WokerFeignClient {
 	
 	 @GetMapping("/{id}")
