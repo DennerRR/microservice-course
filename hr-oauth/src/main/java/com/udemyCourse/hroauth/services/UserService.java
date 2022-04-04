8bb9a198b213a2ab853aecb5a8dde4ad682a7d5f
Denner <dennerramiror@gmail.com>
2022-04-04 12:37:33 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/udemyCourse/hroauth/services/UserService.java
@@ -0,0 +1,28 @@
+package com.udemyCourse.hroauth.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemyCourse.hroauth.entities.User;
+import com.udemyCourse.hroauth.feignclients.UserFeignClient;
+
+@Service
+public class UserService {
+
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		User user = userFeignClient.findByEmail(email).getBody();
+		if (user == null) {
+			logger.error("Email not Found:" + email);
+			throw new IllegalArgumentException("Email not found");
+		}
+		logger.info("Email found: "+ email);
+		return user;
+	}
+}
